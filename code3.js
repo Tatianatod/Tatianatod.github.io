f06b21afbd6860c78bb084f32908cd46b1880a99
Tatiana Todoseva <59057635+Tatianatod@users.noreply.github.com>
2025-06-17 12:58:57 +0200

    delete "slider"

--- code3.js
+++ code3.js
@@ -3,8 +3,7 @@ function showProgress(points) {
     let percent = Math.min((points / maxPoints) * 100, 100);
 
     document.getElementById("progress_bar").style.width = percent + "%";
-    document.getElementById("slider").style.left = percent + "%";
-
+    
     let youAreHere = document.getElementById("you_are_here");
     if (points >= 200 && points < 300) {
         youAreHere.style.display = "inline";

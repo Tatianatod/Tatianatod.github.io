12c437179b732ac136792e009ef6864b0d7bb96f
Tatiana Todoseva <59057635+Tatianatod@users.noreply.github.com>
2025-06-25 22:42:28 +0200

    fix progress-bar

--- code3.js
+++ code3.js
@@ -1,9 +1,19 @@
 function showProgress(points) {
-    let maxPoints = 400;
-    let percent = Math.min((points / maxPoints) * 100, 100);
-
-    document.getElementById("progress_bar").style.width = percent + "%";
+    let progressBar = document.getElementById("progress_bar");
+    let percent = 0;
     
+    if (points <= 200) {
+        percent = (points / 200) * 50;
+    } else if (points <= 300) {
+        percent = 50 + ((points - 200) / 100) * 25;
+    } else if (points <= 400) {
+        percent = 75 + ((points - 300) / 100) * 25;
+    } else {
+        percent = 100;
+    }
+    progressBar.style.width = percent + "%";
+
+
     let youAreHere = document.getElementById("you_are_here");
     if (points >= 200 && points < 300) {
         youAreHere.style.display = "inline";
